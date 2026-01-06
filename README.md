5ad452b95872349f1125ec6f65732cfc9f349568
Sam Leavin <sam.leavin@gmail.com>
2026-01-05 23:20:08 -0500

    updates

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 [hit detection](https://medium.com/@lavrton/hit-region-detection-for-html5-canvas-and-how-to-listen-to-click-events-on-canvas-shapes-815034d7e9f8)
 
 [context2D](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D)
+
+[parametric circle](https://www.mathopenref.com/coordparamcircle.html)
--- src/main.mjs
+++ src/main.mjs
@@ -1,9 +1,13 @@
-const offset = 150.5
-const twoPi = () => 2 * Math.PI;
+const degToRad = deg => deg * Math.PI / 180
+const parametricX = (originX, radius, radians) => originX + (radius * Math.cos(radians))
+const parametricY = (originY, radius, radians) => originY - (radius * Math.sin(radians))
+const twoPi = 2 * Math.PI;
+const midPoint = 150.5
+const radius = 101
+const innerRadius = radius - 25;
 
+/** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('wind-meter')
-
-/** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext("2d")
 
 const setStroke = color => () => ctx.strokeStyle = color
@@ -15,10 +19,9 @@ const fillGreen = setFill('green')
 
 const drawCircle = () => {
     ctx.beginPath();
-    const x = offset;
-    const y = offset
-    const radius = 101
-    ctx.arc(x, y, radius, 0, twoPi(), true)
+    const x = midPoint;
+    const y = midPoint
+    ctx.arc(x, y, radius, 0, twoPi, true)
     ctx.stroke()
 }
 
@@ -26,30 +29,42 @@ const drawCrosshair = () => {
     const length = 9
     const halfLength = length / 2
     ctx.beginPath()
-    ctx.moveTo(offset, offset - halfLength)
-    ctx.lineTo(offset, offset + halfLength)
+    ctx.moveTo(midPoint, midPoint - halfLength)
+    ctx.lineTo(midPoint, midPoint + halfLength)
     ctx.stroke()
     ctx.beginPath()
-    ctx.moveTo(offset - halfLength, offset)
-    ctx.lineTo(offset + halfLength, offset)
+    ctx.moveTo(midPoint - halfLength, midPoint)
+    ctx.lineTo(midPoint + halfLength, midPoint)
+    ctx.stroke()
+}
+
+const drawArrow = () => {
+    ctx.beginPath()
+    ctx.moveTo(midPoint, midPoint - innerRadius)
+    const fiveEighths = twoPi * (5/8)
+    const pointOneX = parametricX(midPoint, innerRadius, fiveEighths)
+    const pointOneY = parametricY(midPoint, innerRadius, fiveEighths)
+    ctx.lineTo(pointOneX, pointOneY)
+    const secondX = parametricX(midPoint, innerRadius, (-1 * Math.PI) / 4 )
+    ctx.lineTo(secondX, pointOneY)
+    ctx.closePath()
     ctx.stroke()
 }
 
 const initialize = () => {
     drawCrosshair()
     drawCircle()
+    drawArrow()
 }
 
-/**
- * @param {MouseEvent} event
- */
+/** @param {MouseEvent} event */
 const canvasClick = (event) => {
     console.log('canvas', event)
     const x = event.clientX - canvas.offsetLeft
     const y = event.clientY - canvas.offsetTop
     // const pixel = ctx.getImageData(mousePos.x, mousePos.y, 1, 1).data;
     ctx.beginPath()
-    ctx.arc(x, y, 10, 0, twoPi(), true)
+    ctx.arc(x, y, 10, 0, twoPi, true)
     fillGreen()
     ctx.fill()
 }
