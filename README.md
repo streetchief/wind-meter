31df62f40ea0bf531db6d9d180f542c559517088
Sam Leavin <sam.leavin@gmail.com>
2026-01-08 14:39:04 -0500

    updates

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# Wind Meter
+
+## Information Sources
 [translate](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/translate)
 
 [hit detection](https://medium.com/@lavrton/hit-region-detection-for-html5-canvas-and-how-to-listen-to-click-events-on-canvas-shapes-815034d7e9f8)
@@ -5,3 +8,17 @@
 [context2D](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D)
 
 [parametric circle](https://www.mathopenref.com/coordparamcircle.html)
+
+[Trig functions](https://en.wikipedia.org/wiki/Trigonometric_functions)
+
+## Math notes
+Quadrants
+
+```
+2 | 1
+-----
+3 | 4
+```
+sine = opp/hyp
+cosine = adj/hyp 
+tangent = opp/adj
--- src/main.mjs
+++ src/main.mjs
@@ -1,18 +1,69 @@
-const degToRad = deg => deg * Math.PI / 180
-const parametricX = (originX, radius, radians) => originX + (radius * Math.cos(radians))
-const parametricY = (originY, radius, radians) => originY - (radius * Math.sin(radians))
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById('wind-meter')
+const ctx = canvas.getContext("2d")
+const angleDisplay = document.getElementById('angle')
+const coordinatesDisplay = document.getElementById('coords')
+const windValue = document.getElementById('wind-value')
+
 const twoPi = 2 * Math.PI;
+const eigths = {
+    1: twoPi / 8,
+    2: twoPi / 4,
+    3: twoPi * (3 / 8),
+    5: twoPi * (5 / 8),
+};
+
+console.log(eigths)
+
 const midPoint = 150.5
 const radius = 101
 const innerRadius = radius - 25;
 
-/** @type {HTMLCanvasElement} */
-const canvas = document.getElementById('wind-meter')
-const ctx = canvas.getContext("2d")
+const degToRad = deg => deg * Math.PI / 180
+const radToDeg = rad => (rad * 180) / Math.PI
+const parametricX = (originX, radius, radians) => {
+    return originX + (radius * Math.cos(radians))
+}
+
+const parametricY = (originY, radius, radians) => {
+    return originY - (radius * Math.sin(radians))
+}
+
+const setAngle = (val = '&Theta;') => {
+    angleDisplay.innerHTML = val;
+}
+
+const setCoords = (x = '-', y = '-') => {
+    coordinatesDisplay.innerText = `x: ${x} y: ${y}`
+}
+
+const calculateQuadrant = (x, y) => {
+    if (x > midPoint) {
+        if (y > midPoint) return 4
+        return 1
+    }
+
+    if (y > midPoint) return 3
+    return 2
+}
+
+/**
+ * Counter-clockwise from +x
+ * @param {number} theta radians from x axis
+ * @param {1|2|3|4} quadrant 
+ * @returns {number}
+ */
+const calculateRotationRadians = (theta, quadrant) => {
+    if (quadrant === 1) return theta
+    if (quadrant === 2) return Math.PI - theta
+    if (quadrant === 3) return Math.PI + theta
+    return twoPi - theta
+}
 
 const setStroke = color => () => ctx.strokeStyle = color
 const strokeBlack = setStroke('black')
 const strokeRed = setStroke('red')
+
 const setFill = color => () => ctx.fillStyle = color
 const fillRed = setFill('red')
 const fillGreen = setFill('green')
@@ -38,14 +89,41 @@ const drawCrosshair = () => {
     ctx.stroke()
 }
 
+/**
+ * 
+ * @param {number} tipX 
+ * @param {number} tipY 
+ */
+const drawRotatedArrow = (tipX, tipY) => {
+    const tipToWing =  eigths[3]
+    const tipToTip = eigths[2]
+    ctx.beginPath()
+    ctx.moveTo(tipX, tipY)
+    const horizontalDiff = Math.abs(tipX - midPoint)
+    const vertDiff = Math.abs(tipY - midPoint)
+    const theta = Math.atan(vertDiff / horizontalDiff)
+    const quadrant = calculateQuadrant(tipX, tipY)
+    const rotation = calculateRotationRadians(theta, quadrant)
+    const p1Rotation = rotation + tipToWing
+    const p1X = parametricX(midPoint, radius, p1Rotation)
+    const p1Y = parametricY(midPoint, radius, p1Rotation)
+    ctx.lineTo(p1X, p1Y)
+    const p2Rotation = p1Rotation + tipToTip
+    const p2X = parametricX(midPoint, radius, p2Rotation)
+    const p2Y = parametricY(midPoint, radius, p2Rotation)
+    ctx.lineTo(p2X, p2Y)
+    ctx.closePath()
+    ctx.stroke()
+}
+
 const drawArrow = () => {
     ctx.beginPath()
     ctx.moveTo(midPoint, midPoint - innerRadius)
-    const fiveEighths = twoPi * (5/8)
+    const fiveEighths = twoPi * (5 / 8)
     const pointOneX = parametricX(midPoint, innerRadius, fiveEighths)
     const pointOneY = parametricY(midPoint, innerRadius, fiveEighths)
     ctx.lineTo(pointOneX, pointOneY)
-    const secondX = parametricX(midPoint, innerRadius, (-1 * Math.PI) / 4 )
+    const secondX = parametricX(midPoint, innerRadius, (-1 * Math.PI) / 4)
     ctx.lineTo(secondX, pointOneY)
     ctx.closePath()
     ctx.stroke()
@@ -54,23 +132,27 @@ const drawArrow = () => {
 const initialize = () => {
     drawCrosshair()
     drawCircle()
-    drawArrow()
 }
+
 
 /** @param {MouseEvent} event */
 const canvasClick = (event) => {
+    // const pixel = ctx.getImageData(mousePos.x, mousePos.y, 1, 1).data;
     console.log('canvas', event)
     const x = event.clientX - canvas.offsetLeft
     const y = event.clientY - canvas.offsetTop
-    // const pixel = ctx.getImageData(mousePos.x, mousePos.y, 1, 1).data;
+    setCoords(x, y)
     ctx.beginPath()
-    ctx.arc(x, y, 10, 0, twoPi, true)
+    ctx.arc(x, y, 5, 0, twoPi, true)
     fillGreen()
     ctx.fill()
+
+    drawRotatedArrow(x, y)
 }
 
-const resetClick = (event) => {
-    console.log('reset', event)
+const resetClick = (_event) => {
+    setCoords()
+    setAngle()
     ctx.reset()
     initialize()
 }
@@ -78,4 +160,4 @@ const resetClick = (event) => {
 strokeBlack()
 initialize()
 canvas.addEventListener('click', canvasClick)
-document.getElementById('reset')?.addEventListener('click', resetClick)
+document.getElementById('reset').addEventListener('click', resetClick)
